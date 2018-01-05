3ff291413f442df0aebf6a6662bc270376966dfb
Justin Tian <tkg8w58@kohls.com>
2018-01-05 11:35:34 -0800

    Adding more notes

--- nltk/nltk_snippets.py
+++ nltk/nltk_snippets.py
@@ -10,3 +10,17 @@ nltk.download('punkt')
 tokens = nltk.word_tokenize(raw)
 
 text = nltk.Text(tokens)
+
+###################################################################################################
+# Get the frequency distribution.
+wsj = sorted(set(nltk.corpus.treebank.words()))
+fd = nltk.FreqDist(vs for word in wsj for vs in re.findall(r'[aeiou]{2,}', word))
+
+###################################################################################################
+# Greedy version of regular expression .*
+re.findall(r'^(.*)(ing|ly|ed|ious|ies|ive|es|s|ment)$', 'processes')
+# [('processe', 's')]
+
+# Non-greedy version of regular expression .*, .*?
+re.findall(r'^(.*?)(ing|ly|ed|ious|ies|ive|es|s|ment)$', 'processes')
+# [('process', 'es')]
