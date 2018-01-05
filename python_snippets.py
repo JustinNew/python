ef32f95706350dfd0bed3da057a74c78be2f5522
Justin Tian <tkg8w58@kohls.com>
2018-01-05 13:06:50 -0800

    python snippets

---
+++ python_snippets.py
@@ -0,0 +1,7 @@
+######################################################################################################
+# Plot and show image in python.
+import pylab
+from matplotlib import pyplot as plt
+
+plt.plot([1,2,3,4], [2,3,4,5])
+pylab.show()
