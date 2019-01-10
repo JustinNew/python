2c94b7617a7d00ee3caa8ee96bfe6a80b0d052a1
Jianhui Tian <jianhui.tian@instacart.com>
2019-01-10 13:33:49 -0800

    datetime

--- python_snippets.py
+++ python_snippets.py
@@ -5,3 +5,12 @@ from matplotlib import pyplot as plt
 
 plt.plot([1,2,3,4], [2,3,4,5])
 pylab.show()
+
+# datetime
+from datetime import datetime
+from datetime import timedelta
+t = datetime(2019,1,2,0,0).date()
+t2 = datetime(2019,1,2,0,0).date() + timedelta(7)
+
+# To '2019-01-02'
+datetime.strftime(t, '%Y-%m-%d')
